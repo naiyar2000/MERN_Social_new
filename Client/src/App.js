06211ab13ffdf3375c8d0aaaa0b2432decb78a6d
naiyar2000 <naiyarimam.2000@gmail.com>
2022-04-15 04:25:20 +0530

    backend and frontend for post crud operation done

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from "react-redux";
+import { getPosts } from './actions/posts';
+import memories from './images/memories.png';
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    const [currentId, setCurrentId] = useState(null);
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
-        <div>
-            app
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memorized</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
+            </AppBar>
+            <Grow in>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId}/>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                    </Grid>
+                </Grid>
+            </Grow>
+        </Container>
     );
 }
 
--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -1,17 +1,40 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
-
-        console.log(postMessages);
-
         res.status(200).send(postMessages);
     } catch (error) {
         res.status(404).json({message: error.message})
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+
+        res.status(200).send(newPost);
+    } catch (error) {
+        res.status(404).send({message: error.message});
+    }
+}
+
+export const updatePost = async(req, res) => {
+    const { id: _id } = req.params;
+    const post  = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("No post with that id");
+        
+    try {
+        const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
+        res.json(updatedPost);
+    } catch (error) {
+        return res.status(404).send({message: error.message});    
+        
+    }
+
 }
