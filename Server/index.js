538651b33ea5594c3fa20a729ff08f083427d679
naiyar2000 <naiyarimam.2000@gmail.com>
2022-04-16 01:24:56 +0530

    dotenv setup changes

--- Server/index.js
+++ Server/index.js
@@ -3,11 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
 import postRoutes from './routes/posts.js'
 
 const app = express();
-dotenv.config();
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -15,9 +14,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+dotenv.config();
+
+//connect to mongodb
+const CONNECTION_URL = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.eophi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((e) => console.log(e.message));
 
